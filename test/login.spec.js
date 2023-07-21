b3197a96c575dc0a4ffa557c96456cc972dc41b0
Amalia Soto <75153007+amalia345@users.noreply.github.com>
2023-07-21 15:35:33 -0600

    test login

--- test/login.spec.js
+++ test/login.spec.js
@@ -1,19 +1,28 @@
+/* eslint-disable no-sequences */
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-undef */
 /* eslint-disable import/extensions */
 /* eslint-disable import/named */
-import { } from 'jest';
+// import { } from 'jest';
+import { expect, jest } from '@jest/globals';
 import { signinUser } from '../src/components/Login.js';
 
-jest.mock('../src/components/Login.js', () => {
-  jest.fn();
-});
-jest.mock('../src/components/Login.js');
-beforeEach(() => {
-  signinUser.mockClear();
-});
-test('user signing', () => {
-  const expectedEmail = { email: 'alexis@gmail.com' };
-  const expectedPassword = { password: '123456' };
-  signinUser.returnValueOnce(expectedEmail, expectedPassword);
-  expected(signinUser).toEqual(expectedEmail, expectedPassword);
+function testsigninUser(email, password) {
+  if (email, password !== 'continue with google') {
+    signinUser(email, password);
+  }
+}
+
+describe('signinUser', () => {
+  test('Test para validar login con email y password', () => {
+    const signInWithEmailAndPassword = jest.fn();
+    testsigninUser(signInWithEmailAndPassword, 'email, password');
+    expect(signInWithEmailAndPassword).toHaveBeenCalled(email, password);
+  });
+
+  test('Test para no validar login', () => {
+    const signInWithEmailAndPassword = jest.fn();
+    testsigninUser(signInWithEmailAndPassword, 'continue with google');
+    expect(signInWithEmailAndPassword).not.toHaveBeenCalled(email, password);
+  });
 });
