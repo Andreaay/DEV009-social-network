0f7f2a739f559b484487a18257394aa0d854a83a
Andrea <andreacabrera37@gmail.com>
2023-08-08 15:54:27 -0600

    test agregado de login agregado

---
+++ test/start.spec.js
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-sequences */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-undef */
+/* eslint-disable import/extensions */
+/* eslint-disable import/named */
+// import { } from 'jest';
+import { expect } from '@jest/globals';
+import { createEvent, bringEvent } from '../src/lib/events';
+// eslint-disable-next-line import/no-cycle
+import { Events } from '../src/components/Events';
+
+describe('Start', () => {
+  beforeEach(() => {
+    const homeDiv = Events();
+    document.body.replaceChildren(homeDiv);
+  });
+  test('Se creó el evento correctamente', () => {
+    const createEvent = document.querySelector('button');
+    expect(createEvent).toBeTruthy();
+  });
+});
