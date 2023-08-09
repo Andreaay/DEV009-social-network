1cfb3c1bc6b0effa74671a7e92e46930daab3c1d
Andrea <andreacabrera37@gmail.com>
2023-08-09 16:39:50 -0600

    descargando correcciones

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,23 +1,53 @@
+import {
+  addDoc, doc, updateDoc, deleteDoc,
+} from 'firebase/firestore';
 import { logOutUser } from '../lib/account';
 import {
-  createEvent, bringEvent,
+  createEvent, bringEvent, updateEvent, removeEvent,
 } from '../lib/events';
-import eventRender from './EventRender';
-import { bottomMenu2, titleBox2 } from './htmlElements';
+import { database } from '../lib/post';
 
 export const Events = (navigateTo) => {
   const homeDiv = document.createElement('div');
+  const title = document.createElement('h1');
   const inputEvent = document.createElement('input');
   inputEvent.id = 'enterEvent';
   const post = document.createElement('p');
   post.innerHTML = 'What is the new event?';
   const buttonShare = document.createElement('button');
   buttonShare.innerHTML = 'share <i class="fa-solid fa-share"></i>';
-
+  const buttonStart = document.createElement('button');
+  buttonStart.innerHTML = '<i class="fas fa-house"></i>';
+  const buttonEvents = document.createElement('button');
+  buttonEvents.innerHTML = '<i class="fas fa-users"></i>';
+  const buttonNewPost = document.createElement('button');
+  buttonNewPost.innerHTML = '<i class="fas fa-plus"></i>';
+  const buttonProfile = document.createElement('button');
+  buttonProfile.innerHTML = '<i class="fas fa-user"></i>';
+  const buttonLogout = document.createElement('button');
+  buttonLogout.innerHTML = '<i class="fa-solid fa-right-from-bracket"></i>';
+  buttonLogout.addEventListener('click', () => {
+    const alertOutUser = (callback) => {
+      if (callback) {
+        navigateTo('/');
+        console.log(alertOutUser);
+      }
+    };
+    logOutUser(alertOutUser);
+  });
+  title.textContent = 'Expressio Music';
+  buttonStart.addEventListener('click', () => {
+    navigateTo('/start');
+  });
+  buttonNewPost.addEventListener('click', () => {
+    navigateTo('/newpost');
+  });
+  buttonProfile.addEventListener('click', () => {
+    navigateTo('/profile');
+  });
   const containerEvents = document.createElement('div');
   containerEvents.classList.add('event-area');
   document.createElement('container', containerEvents);
-
   bringEvent();
   buttonShare.addEventListener('click', () => {
     const valuePost = inputEvent.value;
@@ -42,15 +72,89 @@ export const Events = (navigateTo) => {
   });
   function refreshBringEvent() {
     bringEvent().then((res) => {
+      containerEvents.innerHTML = '';
+      res.forEach((ev) => {
+        console.log(ev.data());
+        const postElement = document.createElement('p');
+        postElement.textContent = ev.data().post;
+        containerEvents.appendChild(postElement);
 
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', (event) => {
+          event.preventDefault();
+          function createEditPopup(initialValue) {
+            const popup = document.createElement('div');
+            popup.classList.add('popup');
+
+            const input = document.createElement('input');
+            input.classList.add('popup-input');
+            input.value = initialValue;
+
+            const saveButton = document.createElement('button');
+            saveButton.classList.add('popup-save');
+            saveButton.textContent = 'Save';
+
+            const cancelButton = document.createElement('button');
+            cancelButton.classList.add('popup-cancel');
+            cancelButton.textContent = 'Cancel';
+
+            popup.appendChild(input);
+            popup.appendChild(saveButton);
+            popup.appendChild(cancelButton);
+
+            return popup;
+          }
+
+          const popup = createEditPopup(ev.data().post);
+
+          const saveButton = popup.querySelector('.popup-save');
+          saveButton.addEventListener('click', () => {
+            const newPostContent = popup.querySelector('.popup-input').value;
+            postElement.textContent = newPostContent;
+            const newData = {
+              post: newPostContent,
+            };
+            if (ev.id) {
+              updateEvent(ev.id, newData);
+            }
+            popup.remove();
+          });
+
+          const cancelButton = popup.querySelector('.popup-cancel');
+          cancelButton.addEventListener('click', () => {
+            popup.remove();
+          });
+
+          containerEvents.appendChild(popup);
+        });
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => {
+          console.log('click');
+          if (ev.id) {
+            removeEvent(ev.id);
+            console.log(removeEvent(ev.id), ev.id);
+          }
+        });
+
+        containerEvents.appendChild(editButton);
+        containerEvents.appendChild(deleteButton);
+      });
     });
   }
   refreshBringEvent();
   window.addEventListener('click', refreshBringEvent);
 
-  homeDiv.append(titleBox2());
-  homeDiv.appendChild(eventRender());
+  homeDiv.append(title);
   homeDiv.append(post, inputEvent, buttonShare);
-  homeDiv.append(bottomMenu2(navigateTo, logOutUser));
+  homeDiv.appendChild(containerEvents);
+  homeDiv.append(buttonStart, buttonEvents, buttonNewPost, buttonProfile, buttonLogout);
+
+  const bottomMenuDiv = document.createElement('div');
+  bottomMenuDiv.classList.add('bottom-menu');
+  bottomMenuDiv.append(buttonStart, buttonEvents, buttonNewPost, buttonProfile, buttonLogout);
+  homeDiv.append(bottomMenuDiv);
   return homeDiv;
 };
